c3902f357c9c164f7ac5443ff5a8728e20d8ff7c
Ubuntu <ubuntu@ip-172-31-1-183.ec2.internal>
2020-11-20 20:40:25 +0000

    Area and perimeter of a rectangle

---
+++ basic_math.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# Created by Sean McLeod
+# Created on November 2020
+# This program can solve basic math
+
+def main():
+    # This function does basic math
+    print("If a rectangle has the dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2".format(5*3))
+    print("Perimeter is {}cm".format(2*(5+3)))
+
+
+if __name__ == "__main__":
+    main()
